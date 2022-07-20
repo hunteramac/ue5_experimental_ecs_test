daa4a92362d3de8710ce3619a13e0ebd35752d4e
Hunter MacNab <hunter.amac@gmail.com>
2022-07-19 19:57:23 -0700

    added destroyOnCollision trait to DA_config

--- simVillage/Intermediate/Build/Win64/UnrealEditor/Inc/simVillage/EntityTrait_destroyOnCollision.gen.cpp
+++ simVillage/Intermediate/Build/Win64/UnrealEditor/Inc/simVillage/EntityTrait_destroyOnCollision.gen.cpp
@@ -13,6 +13,9 @@ void EmptyLinkFunctionForGeneratedCodeEntityTrait_destroyOnCollision() {}
 	SIMVILLAGE_API UClass* Z_Construct_UClass_UEntityTrait_destroyOnCollision();
 	MASSSPAWNER_API UClass* Z_Construct_UClass_UMassEntityTraitBase();
 	UPackage* Z_Construct_UPackage__Script_simVillage();
+	SIMVILLAGE_API UClass* Z_Construct_UClass_UProcessor_checkCollision_NoRegister();
+	SIMVILLAGE_API UClass* Z_Construct_UClass_UProcessor_checkCollision();
+	MASSENTITY_API UClass* Z_Construct_UClass_UMassProcessor();
 // End Cross Module References
 	void UEntityTrait_destroyOnCollision::StaticRegisterNativesUEntityTrait_destroyOnCollision()
 	{
@@ -73,14 +76,73 @@ void EmptyLinkFunctionForGeneratedCodeEntityTrait_destroyOnCollision() {}
 		return UEntityTrait_destroyOnCollision::StaticClass();
 	}
 	DEFINE_VTABLE_PTR_HELPER_CTOR(UEntityTrait_destroyOnCollision);
+	void UProcessor_checkCollision::StaticRegisterNativesUProcessor_checkCollision()
+	{
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(UProcessor_checkCollision);
+	UClass* Z_Construct_UClass_UProcessor_checkCollision_NoRegister()
+	{
+		return UProcessor_checkCollision::StaticClass();
+	}
+	struct Z_Construct_UClass_UProcessor_checkCollision_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_UProcessor_checkCollision_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_UMassProcessor,
+		(UObject* (*)())Z_Construct_UPackage__Script_simVillage,
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UProcessor_checkCollision_Statics::Class_MetaDataParams[] = {
+		{ "IncludePath", "EntityTrait_destroyOnCollision.h" },
+		{ "ModuleRelativePath", "Public/EntityTrait_destroyOnCollision.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_UProcessor_checkCollision_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<UProcessor_checkCollision>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_UProcessor_checkCollision_Statics::ClassParams = {
+		&UProcessor_checkCollision::StaticClass,
+		"Mass",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x000030A6u,
+		METADATA_PARAMS(Z_Construct_UClass_UProcessor_checkCollision_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_UProcessor_checkCollision_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_UProcessor_checkCollision()
+	{
+		if (!Z_Registration_Info_UClass_UProcessor_checkCollision.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_UProcessor_checkCollision.OuterSingleton, Z_Construct_UClass_UProcessor_checkCollision_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_UProcessor_checkCollision.OuterSingleton;
+	}
+	template<> SIMVILLAGE_API UClass* StaticClass<UProcessor_checkCollision>()
+	{
+		return UProcessor_checkCollision::StaticClass();
+	}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(UProcessor_checkCollision);
 	struct Z_CompiledInDeferFile_FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_Statics
 	{
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
 	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_Statics::ClassInfo[] = {
 		{ Z_Construct_UClass_UEntityTrait_destroyOnCollision, UEntityTrait_destroyOnCollision::StaticClass, TEXT("UEntityTrait_destroyOnCollision"), &Z_Registration_Info_UClass_UEntityTrait_destroyOnCollision, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UEntityTrait_destroyOnCollision), 2611366264U) },
+		{ Z_Construct_UClass_UProcessor_checkCollision, UProcessor_checkCollision::StaticClass, TEXT("UProcessor_checkCollision"), &Z_Registration_Info_UClass_UProcessor_checkCollision, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UProcessor_checkCollision), 674075340U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_1841515688(TEXT("/Script/simVillage"),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_3816762791(TEXT("/Script/simVillage"),
 		Z_CompiledInDeferFile_FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
--- simVillage/Intermediate/Build/Win64/UnrealEditor/Inc/simVillage/EntityTrait_destroyOnCollision.generated.h
+++ simVillage/Intermediate/Build/Win64/UnrealEditor/Inc/simVillage/EntityTrait_destroyOnCollision.generated.h
@@ -13,10 +13,10 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define SIMVILLAGE_EntityTrait_destroyOnCollision_generated_h
 
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_SPARSE_DATA
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_RPC_WRAPPERS
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_RPC_WRAPPERS_NO_PURE_DECLS
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_INCLASS_NO_PURE_DECLS \
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_SPARSE_DATA
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_RPC_WRAPPERS
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesUEntityTrait_destroyOnCollision(); \
 	friend struct Z_Construct_UClass_UEntityTrait_destroyOnCollision_Statics; \
@@ -25,7 +25,7 @@ public: \
 	DECLARE_SERIALIZER(UEntityTrait_destroyOnCollision)
 
 
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_INCLASS \
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_INCLASS \
 private: \
 	static void StaticRegisterNativesUEntityTrait_destroyOnCollision(); \
 	friend struct Z_Construct_UClass_UEntityTrait_destroyOnCollision_Statics; \
@@ -34,7 +34,7 @@ public: \
 	DECLARE_SERIALIZER(UEntityTrait_destroyOnCollision)
 
 
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_STANDARD_CONSTRUCTORS \
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API UEntityTrait_destroyOnCollision(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UEntityTrait_destroyOnCollision) \
@@ -47,7 +47,7 @@ private: \
 public:
 
 
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_ENHANCED_CONSTRUCTORS \
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_ENHANCED_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API UEntityTrait_destroyOnCollision(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
 private: \
@@ -60,30 +60,100 @@ public: \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UEntityTrait_destroyOnCollision)
 
 
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_12_PROLOG
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_GENERATED_BODY_LEGACY \
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_17_PROLOG
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_SPARSE_DATA \
-	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_RPC_WRAPPERS \
-	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_INCLASS \
-	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_STANDARD_CONSTRUCTORS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_SPARSE_DATA \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_RPC_WRAPPERS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_INCLASS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_GENERATED_BODY \
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_SPARSE_DATA \
-	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
-	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_INCLASS_NO_PURE_DECLS \
-	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_15_ENHANCED_CONSTRUCTORS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_SPARSE_DATA \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_INCLASS_NO_PURE_DECLS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_20_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
 template<> SIMVILLAGE_API UClass* StaticClass<class UEntityTrait_destroyOnCollision>();
+
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_SPARSE_DATA
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_RPC_WRAPPERS
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUProcessor_checkCollision(); \
+	friend struct Z_Construct_UClass_UProcessor_checkCollision_Statics; \
+public: \
+	DECLARE_CLASS(UProcessor_checkCollision, UMassProcessor, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/simVillage"), NO_API) \
+	DECLARE_SERIALIZER(UProcessor_checkCollision)
+
+
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_INCLASS \
+private: \
+	static void StaticRegisterNativesUProcessor_checkCollision(); \
+	friend struct Z_Construct_UClass_UProcessor_checkCollision_Statics; \
+public: \
+	DECLARE_CLASS(UProcessor_checkCollision, UMassProcessor, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/simVillage"), NO_API) \
+	DECLARE_SERIALIZER(UProcessor_checkCollision)
+
+
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UProcessor_checkCollision(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UProcessor_checkCollision) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UProcessor_checkCollision); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UProcessor_checkCollision); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UProcessor_checkCollision(UProcessor_checkCollision&&); \
+	NO_API UProcessor_checkCollision(const UProcessor_checkCollision&); \
+public:
+
+
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UProcessor_checkCollision(UProcessor_checkCollision&&); \
+	NO_API UProcessor_checkCollision(const UProcessor_checkCollision&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UProcessor_checkCollision); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UProcessor_checkCollision); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UProcessor_checkCollision)
+
+
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_25_PROLOG
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_SPARSE_DATA \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_RPC_WRAPPERS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_INCLASS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_SPARSE_DATA \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_INCLASS_NO_PURE_DECLS \
+	FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h_28_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SIMVILLAGE_API UClass* StaticClass<class UProcessor_checkCollision>();
 
 #undef CURRENT_FILE_ID
 #define CURRENT_FILE_ID FID_simVillage_Source_simVillage_Public_EntityTrait_destroyOnCollision_h
