95b067581bb96da6c436abe86d24cb91b7779ad3
Hunter MacNab <hunter.amac@gmail.com>
2022-07-19 19:53:09 -0700

    created EntityTrait_destroyOnCollision
     Scafold class created to implement ECS collisions with player controlled actors.
    Trait to add to ECS EntityTrait_destroyOnCollision.
    Detect fp player gun projectiles and delete entity. Goal to implement MVP of player interaction with ECS Mass Entity System.

--- simVillage/Intermediate/Build/Win64/UnrealEditor/Inc/simVillage/simVillage.init.gen.cpp
+++ simVillage/Intermediate/Build/Win64/UnrealEditor/Inc/simVillage/simVillage.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodesimVillage_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x0292112E,
-				0x535909C5,
+				0x7815B49E,
+				0x759E3CBB,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_simVillage.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_simVillage.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_simVillage(Z_Construct_UPackage__Script_simVillage, TEXT("/Script/simVillage"), Z_Registration_Info_UPackage__Script_simVillage, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x0292112E, 0x535909C5));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_simVillage(Z_Construct_UPackage__Script_simVillage, TEXT("/Script/simVillage"), Z_Registration_Info_UPackage__Script_simVillage, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x7815B49E, 0x759E3CBB));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
